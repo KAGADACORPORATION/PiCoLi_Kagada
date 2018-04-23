f2dd4b430a42e4efc589989c3aeead120b26e8e0
manufuentes98 <36626964+manufuentes98@users.noreply.github.com>
2018-04-23 13:43:47 +0200

    Puente hecho (ﾉ･o･)ﾉ

--- src/control/Puente.java
+++ src/control/Puente.java
@@ -1,5 +1,43 @@
 package control;
 
-public class Puente {
+import acciones.BarajarColor;
+import acciones.BorrarColor;
+import acciones.IniciarPrograma;
+import acciones.PedirColor;
+import acciones.SeleccionarColor;
+import modelo.Datos;
+import vista.VistaPrincipal;
+
+public class Puente extends VistaPrincipal {
+	private VistaPrincipal vistaPrincipal;
+	private SeleccionarColor seleccionarColor;
+	private PedirColor pedirColor;
+	private Logica logica;
+	private BarajarColor barajarColor;
+	private BorrarColor borrarColor;
+	private Datos datos;
+
+	public Puente() {
+		super();
+		IniciarPrograma iniciador = new IniciarPrograma();
+		datos = iniciador.crearDatos();
+		this.vistaPrincipal = new VistaPrincipal();
+		this.logica = new Logica();
+		
+		this.seleccionarColor = new SeleccionarColor(this.panelSeleccionarColor, this.botonSeleccionarColor,
+				this.datos);
+		this.barajarColor = new BarajarColor(this.datos);
+		this.borrarColor= new BorrarColor(this.datos);
+		this.pedirColor= new PedirColor(this.panelSeleccionarColor, this.botonSeleccionarColor,
+				this.datos);
+		
+		this.botonSeleccionarColor.addActionListener(seleccionarColor);
+		this.botonPedirColor.addActionListener(pedirColor);
+		this.botonBorrarColor.addActionListener(borrarColor);
+		this.botonBarajarPila.addActionListener(barajarColor);
+		
+	
+
+	}
 
 }

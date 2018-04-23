d53f16fdfd7e3394b215cada66e17be8d1c7aec6
OscarPS11 <oscarps11yt@gmail.com>
2018-04-23 10:44:34 +0200

    colores y rellenar cola

--- src/acciones/IniciarPrograma.java
+++ src/acciones/IniciarPrograma.java
@@ -1,3 +1,40 @@
 package acciones;
-public class IniciarPrograma extends Accion {
+
+import java.awt.Color;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+
+import interfaces.Iniciable;
+import modelo.Cola;
+import modelo.Colores;
+import modelo.Lista;
+import modelo.Pila;
+
+public class IniciarPrograma extends Accion implements Iniciable {
+	Cola cola;
+	Pila pila;
+	Lista lista;
+
+	@Override
+	public void iniciarPrograma() {
+		cola = new Cola(new ArrayDeque<>());
+		for (int i = 0; i < utiles.Constantes.TAMANO_COLA; i++) {
+			int numero = genNumeroRandom();
+			cola.cola.add(Colores.getElement(numero));
+		}
+		
+		pila = new Pila();
+		lista = new Lista();
+	}
+
+	private int genNumeroRandom() {
+		return (int) (Math.random() * 5);
+	}
+
+	@Override
+	public void mostrarCambios() {
+		// TODO Auto-generated method stub
+
+	}
+
 }
---
+++ src/modelo/Colores.java
@@ -0,0 +1,6 @@
+package modelo;
+
+enum Color {
+
+  
+}

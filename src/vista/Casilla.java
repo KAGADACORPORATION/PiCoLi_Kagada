7ded4cf0f46a48d513b4e14fa4f43980011f370e
manufuentes98 <36626964+manufuentes98@users.noreply.github.com>
2018-04-23 13:51:18 +0200

    Casilla terminada! ( ・_・)ノ

--- src/vista/Casilla.java
+++ src/vista/Casilla.java
@@ -1,6 +1,25 @@
 package vista;
-public class Casilla {
+
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+import javax.swing.SizeSequence;
+import javax.swing.border.LineBorder;
+
+import modelo.Colores;
+
+public class Casilla extends JLabel {
 
   public String Color;
+
+public Casilla(String color) {
+	super();
+	Color = color;
+	this.setMaximumSize(new Dimension(500, 500));
+	JLabel NuevaCasilla= new JLabel(" ");
+	NuevaCasilla.setBorder(new LineBorder(new java.awt.Color(0, 0, 0),2,true));
+	NuevaCasilla.setOpaque(true);
+	NuevaCasilla.setBackground(Colores.valueOf(color).getColor());
+}
 
 }

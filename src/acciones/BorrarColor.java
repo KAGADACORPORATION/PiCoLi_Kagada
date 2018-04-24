f57d4b016b8ae6444b8afbcced6ad7ddf0ed420d
Maroto97 <mariobasket1997@gmail.com>
2018-04-24 09:43:59 +0200

    BorrarColor
    BorrarColor casi completo

--- src/acciones/BorrarColor.java
+++ src/acciones/BorrarColor.java
@@ -1,13 +1,39 @@
 package acciones;
 
+import java.awt.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Iterator;
+import javax.swing.JLabel;
+import interfaces.Borrable;
+import modelo.Datos;
+import vista.Casilla;
 
-public class BorrarColor implements ActionListener {
-
+public class BorrarColor implements ActionListener, Borrable {
+	Actualizador actualizador;
+	Datos datos;
+	public BorrarColor(Datos datos, Actualizador actualizador) {
+		super();
+		this.datos = datos;
+		this.actualizador = actualizador;
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Apéndice de método generado automáticamente
-		
+		Casilla etiquetaActual = ((Casilla)e.getSource());
+		borrarColor(etiquetaActual.getColor(),datos);
 	}
+	//QUEDA PENDIENTE DE LA LISTA
+	@Override
+	public void borrarColor(String color,Datos datos) {
+		for (int i = 0; i < datos.lista.Lista.size(); i++) {
+			datos.lista.Lista.remove(color);
+			System.out.println(Integer.valueOf(datos.lista.Lista.size()));
+		}
+	}
+
+	@Override
+	public void actualizar() {
+		this.actualizador.actualizar();
+	}
+
 }

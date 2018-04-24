4573677ad98a671d57598cb47497ab0c60f2c592
OscarPS11 <oscarps11yt@gmail.com>
2018-04-24 09:54:13 +0200

    cambio lista

--- src/modelo/Lista.java
+++ src/modelo/Lista.java
@@ -1,9 +1,15 @@
 package modelo;
 
 import java.awt.List;
+import java.util.ArrayList;
 
 public class Lista {
 
-  public List Lista;
+  public ArrayList lista= new ArrayList();
+
+public Lista(ArrayList lista) {
+	super();
+	lista = lista;
+}
 
 }

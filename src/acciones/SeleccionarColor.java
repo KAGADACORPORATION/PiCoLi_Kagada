ef8199cfe89700b107c5173d9786b7df166f085b
manufuentes98 <36626964+manufuentes98@users.noreply.github.com>
2018-04-24 08:24:11 +0200

    SeleccionarColor actualizado ʕ·ᴥ·　ʔ

--- src/acciones/SeleccionarColor.java
+++ src/acciones/SeleccionarColor.java
@@ -3,11 +3,42 @@ package acciones;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import modelo.Datos;
+import utiles.Constantes;
+import vista.Casilla;
+
 public class SeleccionarColor implements ActionListener{
+ private JPanel panelSeleccionarColor;
+ private JButton botonSeleccionarColor;
+ private Datos datos;
+ 
+ 
+ 
+	public SeleccionarColor(JPanel panelSeleccionarColor, JButton botonSeleccionarColor, Datos datos) {
+	super();
+	this.panelSeleccionarColor = panelSeleccionarColor;
+	this.botonSeleccionarColor = botonSeleccionarColor;
+	this.datos = datos;
+}
+
+
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Apéndice de método generado automáticamente
-		
+	panelSeleccionarColor.remove(botonSeleccionarColor);
+	for (int i = 0; i < Constantes.PETICION_COLOR_MAX; i++) {
+		Casilla casillaActual= new Casilla(obtenerColorValido());
+		panelSeleccionarColor.add(casillaActual);
+	}
+	
+	}
+
+	//METODOS
+	private String obtenerColorValido() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
